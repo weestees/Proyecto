92b075ba451feb815cd8c651da149621847cc0a8
Alex <alex.calderon@epn.edu.ec>
2024-11-27 12:40:46 -0500

    Menus restantes

---
+++ src/MenuVendedor.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import './Menu.css';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+
+      <div className="main-container">
+        <main className="app-main">
+          <h2>Menú de Opciones</h2>
+          <div className="menu-options">
+            <div className="menu-item">
+              <button className="menu-button">
+                <span className="icon">➕</span>
+                <span>Gestion de Pedidos</span>
+              </button>
+            </div>
+            <div className="menu-item">
+              <button className="menu-button">
+                <span className="icon">🛒</span>
+                <span>Inventario</span>
+              </button>
+            </div>
+            <div className="menu-item">
+              <button className="menu-button">
+                <span className="icon">💳</span>
+                <span>Transacciones</span>
+              </button>
+            </div>
+          </div>
+        </main>
+      </div>
+      </div>
+
+
+  );
+}
+
+export default App;
